06e55447b477b53834ca7c57adc676c11ef17828
Unknown <romain.desajardim@gmail.com>
2017-10-14 02:39:23 +0200

    Add bundle class

---
+++ BroadwayGeneratorBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RomainDeSaJardim\Bundle\BroadwayGeneratorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+/**
+ * BroadwayGeneratorBundle.
+ *
+ * @author Romain De Sa Jardim <romain.desajardim@gmail.com>
+ */
+class BroadwayGeneratorBundle extends Bundle
+{
+}
