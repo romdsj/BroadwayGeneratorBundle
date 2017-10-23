2f2602302f409ab08da40fde82e14012e32ee6af
Unknown <romain.desajardim@gmail.com>
2017-10-23 16:19:30 +0200

    Add broadwayBunde to requirements

---
+++ Manipulator/CommandHandlerManipulator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romaindesajardim
+ * Date: 23/10/2017
+ * Time: 14:03
+ */
+
+namespace RomainDeSaJardim\Bundle\BroadwayGeneratorBundle\Manipulator;
+
+
+class CommandHandlerManipulator
+{
+
+}
